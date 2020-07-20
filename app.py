658cc7e5d32106f62c99ea78c5753fe2345b6e4d
Jiaxuan Lin <mike200177@hotmail.com>
2020-07-20 22:08:03 +0800

    匯入Json、修改介面

--- app.py
+++ app.py
@@ -1,10 +1,14 @@
 from flask import Flask, render_template
+import json
 
 app = Flask(__name__)
 
+with open(r'static\json\courseData.json', 'r', encoding='utf-8-sig') as jsonFile:
+    courseData = json.load(jsonFile)
+
 @app.route('/')
 def index():
-    return render_template('index.html')
+    return render_template('index.html', courseData=courseData)
 
 if __name__ == "__main__":
     app.run(debug=True)
