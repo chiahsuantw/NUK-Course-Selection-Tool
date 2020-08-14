c48965acf2f175d048bcb5fc8fe25c5af41c3a0a
Jiaxuan Lin <mike200177@hotmail.com>
2020-08-14 19:05:28 +0800

    整合個人修課紀錄爬蟲

--- app.py
+++ app.py
@@ -1,17 +1,25 @@
-from flask import Flask, render_template, request, redirect, url_for, flash
+from flask import Flask, render_template, request, redirect, url_for, flash, make_response
+from scraper import set_user_data, get_student_course, get_student_progress
 import requests
 import json
+import hashlib
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = '3c178af81e8f023e05fc72c56757417158aaeff46e23263df647d9fdc4ad2452'
 
-ENV = 'PROD'
+ENV = 'DEV'
 
-with open('static\\json\\studentCourse.json', 'r', encoding='utf-8-sig') as jsonFile1:
-    studentCourseData = json.load(jsonFile1)
+# def user_account():
+#     return request.cookies.get('Account')
 
-with open('static\\json\\student_credits.json', 'r', encoding='utf-8-sig') as jsonFile3:
-    studentCourseCredits = json.load(jsonFile3)
+# def user_password():
+#     return request.cookies.get('Password')
+
+# with open('static\\json\\studentCourse.json', 'r', encoding='utf-8-sig') as jsonFile1:
+#     studentCourseData = json.load(jsonFile1)
+
+# with open('static\\json\\student_credits.json', 'r', encoding='utf-8-sig') as jsonFile3:
+#     studentCourseCredits = json.load(jsonFile3)
 
 if ENV == 'DEV':
     with open('static\\json\\course.json', 'r', encoding='utf-8-sig') as jsonFile2:
@@ -20,7 +28,6 @@ else:
     url = 'https://e7401c50-a59b-4ef9-9a40-a5ddf3852603.mock.pstmn.io/courseData'
     response = requests.get(url)
     courseData = json.loads(response.text)
-
 
 @app.route('/')
 def index():
@@ -44,7 +51,10 @@ def login():
     res.encoding = 'big5'
 
     if res.headers['Content-Length'] != '992':
-        return redirect(url_for('profile'))
+        response = make_response(redirect(url_for('profile')))
+        response.set_cookie('Account', account)
+        response.set_cookie('Password', password)
+        return response
     else:
         flash('教務系統說登入錯誤', 'danger')
         return redirect(url_for('login'))
@@ -55,8 +65,27 @@ def logout():
     return 'logout'
 
 
+# @app.route('/delcookie')
+# def delete_cookie():
+#     if request.cookies.get('TeamName') != None:
+#         res = make_response(redirect(url_for('home')))
+#         res.delete_cookie('TeamName')
+#         res.delete_cookie('User')
+#         return res
+#     else:
+#         return redirect(url_for('home'))
+
+
 @app.route('/profile')
 def profile():
+    cookieAccount = request.cookies.get('Account')
+    cookiePassword = request.cookies.get('Password')
+    set_user_data(cookieAccount, cookiePassword)
+
+    studentCourseData = get_student_course()
+    studentCourseCredits = get_student_progress()
+    if cookieAccount == None or cookiePassword == None:
+        return redirect(url_for('login'))
     return render_template('profile.html', studentCourseData=studentCourseData, studentCourseCredits=studentCourseCredits)
 
 
