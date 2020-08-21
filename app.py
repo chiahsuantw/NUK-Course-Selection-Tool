67121340b053a1ca54178cbacec9ce17f2f89402
Jiaxuan Lin <mike200177@hotmail.com>
2020-08-21 15:55:13 +0800

    網站驗證、首頁增加 Meta Tag

--- app.py
+++ app.py
@@ -4,18 +4,14 @@ import requests
 import json
 import hashlib
 
+
 app = Flask(__name__)
 app.config['SECRET_KEY'] = '3c178af81e8f023e05fc72c56757417158aaeff46e23263df647d9fdc4ad2452'
 
-ENV = 'PROD'
-
-if ENV == 'DEV':
-    with open('static\\json\\course.json', 'r', encoding='utf-8-sig') as jsonFile2:
-        courseData = json.load(jsonFile2)
-else:
-    url = 'https://e7401c50-a59b-4ef9-9a40-a5ddf3852603.mock.pstmn.io/courseData'
-    response = requests.get(url)
-    courseData = json.loads(response.text)
+# get course data from API sever
+url = 'https://e7401c50-a59b-4ef9-9a40-a5ddf3852603.mock.pstmn.io/courseData'
+response = requests.get(url)
+courseData = json.loads(response.text)
 
 
 @app.route('/')
